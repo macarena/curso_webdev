40100b8a99901dd20c0015b6c7e67a8312b5a56a
Marco Aurélio de Araújo Silva <marcoaasilva@gmail.com>
2016-10-24 14:49:29 -0200

    forca finalizado

--- forca.py
+++ forca.py
@@ -2,23 +2,31 @@ import random
 
 lista = ['banana', 'abacate', 'futebol', 'amor', 'facebook', 'cachorro' ]
 
-palavra = 'abacate'
+palavra = lista[random.randint(0,len(lista)-1)]
 
-print "_ " * len(palavra)
+palavra_escondida = "_ " * len(palavra)
+print palavra_escondida
 
 erros = 0
+tentativas = 10
 
-letra_chutada = raw_input('Chute uma letra: ')
-
-tem_letra = False
-
-for letra in palavra:
-	if letra == letra_chutada:
-		tem_letra = True
-
-if tem_letra:
-	print 'Acertou!'
-else:
-	erros = +1
-
-print erros
+while tentativas > 0:
+	
+	letra_chutada = raw_input('Chute uma letra: ')
+	
+	tem_letra = False
+	
+	for pos in range(len(palavra)):
+		if palavra[pos] == letra_chutada:
+			tem_letra = True
+			palavra_escondida = '{0}{1}{2}'.format(palavra_escondida[:pos*2], letra_chutada, palavra_escondida[(pos + 1)*2:])
+	
+	if tem_letra:
+		print 'Acertou!'
+		print palavra_escondida
+	else:
+		erros = erros + 1
+		tentativas = tentativas - 1
+		print erros
+	
+print "FIM"
